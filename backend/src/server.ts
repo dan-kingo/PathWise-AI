5a3ad6c86c5b2a25ca55b5f2a68fed453b2ceb14
Dan Kingo <danieldejen23@gmail.com>
2025-07-04 17:11:22 +0300

    implement db connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,21 @@
-import express, {Request, Response} from 'express'
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import connectDB from "./configs/db.js";
 
-const app = express()
-const PORT = process.env.PORT || 3000
+dotenv.config();
 
-app.get("/" ,(_req:Request, res:Response) => {
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-  res.send("The backend is running successfully!")
+app.use(express.json());
 
-})
+// DB connection
+connectDB();
+
+app.get("/", (_req: Request, res: Response) => {
+  res.send("The backend is running successfully!");
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
-}) 
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
