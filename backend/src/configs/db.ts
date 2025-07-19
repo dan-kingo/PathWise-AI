49b9c402bd8a0a31db244939d5c655c1eecba308
Dan Kingo <danieldejen23@gmail.com>
2025-07-19 22:02:49 +0300

    fix issues for deployment

--- backend/src/configs/db.ts
+++ backend/src/configs/db.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI_LOCAL!);
+    const conn = await mongoose.connect(process.env.MONGO_URI!);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error("MongoDB connection failed:", error);
