e1d2d0101c78ab80d6fb794cf7fd7d443ad87855
Dan Kingo <danieldejen23@gmail.com>
2025-07-20 12:59:55 +0300

    fix issues in passport config

--- backend/src/configs/passport.ts
+++ backend/src/configs/passport.ts
@@ -10,7 +10,7 @@ passport.use(new GoogleStrategy(
   {
     clientID: process.env.GOOGLE_CLIENT_ID!,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    callbackURL: "/auth/google/callback",
+    callbackURL: process.env.GOOGLE_CALLBACK_URL!,
   }, 
   async (_, __, profile, done) => {
     try {
